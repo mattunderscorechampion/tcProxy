e7d36b5469693af405cc307ee3726e5b557d232d
Matt Champion <mattunderscorechampion@gmail.com>
2015-10-21 20:30:38 +0100

    Increased test coverage for IOSocketFactoryImpl.

--- tcProxy-io-jsl/src/test/java/com/mattunderscore/tcproxy/io/impl/IOSocketFactoryImplTest.java
+++ tcProxy-io-jsl/src/test/java/com/mattunderscore/tcproxy/io/impl/IOSocketFactoryImplTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import java.io.IOException;
+import java.net.SocketAddress;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,6 +23,8 @@ public final class IOSocketFactoryImplTest {
     private IOFactory ioFactory;
     @Mock
     private IOSocketChannel socketChannel;
+    @Mock
+    private SocketAddress address;
 
     @Before
     public void setUp() throws IOException {
@@ -89,6 +92,121 @@ public final class IOSocketFactoryImplTest {
         verify(ioFactory).openSocket();
         verify(socketChannel).set(IOSocketOption.RECEIVE_BUFFER, 1024);
         verify(socketChannel).set(IOSocketOption.BLOCKING, false);
+        verify(socketChannel).set(IOSocketOption.REUSE_ADDRESS, false);
+        verify(socketChannel).set(IOSocketOption.KEEP_ALIVE, false);
+        verify(socketChannel).set(IOSocketOption.TCP_NO_DELAY, false);
+        verify(socketChannel).bind(null);
+        verifyNoMoreInteractions(socketChannel, ioFactory);
+    }
+
+    @Test
+    public void createWithSendBuffer() throws IOException {
+        final IOSocketFactory<IOSocketChannel> factory = new IOSocketFactoryImpl(ioFactory);
+
+        final IOSocketChannel channel = factory.sendBuffer(1024).create();
+
+        assertEquals(socketChannel, channel);
+        verify(ioFactory).openSocket();
+        verify(socketChannel).set(IOSocketOption.SEND_BUFFER, 1024);
+        verify(socketChannel).set(IOSocketOption.BLOCKING, true);
+        verify(socketChannel).set(IOSocketOption.REUSE_ADDRESS, false);
+        verify(socketChannel).set(IOSocketOption.KEEP_ALIVE, false);
+        verify(socketChannel).set(IOSocketOption.TCP_NO_DELAY, false);
+        verify(socketChannel).bind(null);
+        verifyNoMoreInteractions(socketChannel, ioFactory);
+    }
+
+    @Test
+    public void createWithKeepAlive() throws IOException {
+        final IOSocketFactory<IOSocketChannel> factory = new IOSocketFactoryImpl(ioFactory);
+
+        final IOSocketChannel channel = factory.keepAlive(true).create();
+
+        assertEquals(socketChannel, channel);
+        verify(ioFactory).openSocket();
+        verify(socketChannel).set(IOSocketOption.BLOCKING, true);
+        verify(socketChannel).set(IOSocketOption.REUSE_ADDRESS, false);
+        verify(socketChannel).set(IOSocketOption.KEEP_ALIVE, true);
+        verify(socketChannel).set(IOSocketOption.TCP_NO_DELAY, false);
+        verify(socketChannel).bind(null);
+        verifyNoMoreInteractions(socketChannel, ioFactory);
+    }
+
+    @Test
+    public void createWithLinger() throws IOException {
+        final IOSocketFactory<IOSocketChannel> factory = new IOSocketFactoryImpl(ioFactory);
+
+        final IOSocketChannel channel = factory.linger(1024).create();
+
+        assertEquals(socketChannel, channel);
+        verify(ioFactory).openSocket();
+        verify(socketChannel).set(IOSocketOption.LINGER, 1024);
+        verify(socketChannel).set(IOSocketOption.BLOCKING, true);
+        verify(socketChannel).set(IOSocketOption.REUSE_ADDRESS, false);
+        verify(socketChannel).set(IOSocketOption.KEEP_ALIVE, false);
+        verify(socketChannel).set(IOSocketOption.TCP_NO_DELAY, false);
+        verify(socketChannel).bind(null);
+        verifyNoMoreInteractions(socketChannel, ioFactory);
+    }
+
+    @Test
+    public void createWithReuseAddress() throws IOException {
+        final IOSocketFactory<IOSocketChannel> factory = new IOSocketFactoryImpl(ioFactory);
+
+        final IOSocketChannel channel = factory.reuseAddress(true).create();
+
+        assertEquals(socketChannel, channel);
+        verify(ioFactory).openSocket();
+        verify(socketChannel).set(IOSocketOption.BLOCKING, true);
+        verify(socketChannel).set(IOSocketOption.REUSE_ADDRESS, true);
+        verify(socketChannel).set(IOSocketOption.KEEP_ALIVE, false);
+        verify(socketChannel).set(IOSocketOption.TCP_NO_DELAY, false);
+        verify(socketChannel).bind(null);
+        verifyNoMoreInteractions(socketChannel, ioFactory);
+    }
+
+    @Test
+    public void createWithNoDelay() throws IOException {
+        final IOSocketFactory<IOSocketChannel> factory = new IOSocketFactoryImpl(ioFactory);
+
+        final IOSocketChannel channel = factory.noDelay(true).create();
+
+        assertEquals(socketChannel, channel);
+        verify(ioFactory).openSocket();
+        verify(socketChannel).set(IOSocketOption.BLOCKING, true);
+        verify(socketChannel).set(IOSocketOption.REUSE_ADDRESS, false);
+        verify(socketChannel).set(IOSocketOption.KEEP_ALIVE, false);
+        verify(socketChannel).set(IOSocketOption.TCP_NO_DELAY, true);
+        verify(socketChannel).bind(null);
+        verifyNoMoreInteractions(socketChannel, ioFactory);
+    }
+
+    @Test
+    public void createBound() throws IOException {
+        final IOSocketFactory<IOSocketChannel> factory = new IOSocketFactoryImpl(ioFactory);
+
+        final IOSocketChannel channel = factory.bind(address).create();
+
+        assertEquals(socketChannel, channel);
+        verify(ioFactory).openSocket();
+        verify(socketChannel).set(IOSocketOption.BLOCKING, true);
+        verify(socketChannel).set(IOSocketOption.REUSE_ADDRESS, false);
+        verify(socketChannel).set(IOSocketOption.KEEP_ALIVE, false);
+        verify(socketChannel).set(IOSocketOption.TCP_NO_DELAY, false);
+        verify(socketChannel).bind(address);
+        verifyNoMoreInteractions(socketChannel, ioFactory);
+    }
+
+    @Test
+    public void createWithLatestReceiveBuffer() throws IOException {
+        final IOSocketFactory<IOSocketChannel> factory = new IOSocketFactoryImpl(ioFactory);
+
+        final IOSocketChannel channel = factory.receiveBuffer(1024).receiveBuffer(2048).create();
+
+        assertEquals(socketChannel, channel);
+        verify(ioFactory).openSocket();
+        verify(socketChannel).set(IOSocketOption.RECEIVE_BUFFER, 2048);
+        verify(socketChannel).set(IOSocketOption.BLOCKING, true);
         verify(socketChannel).set(IOSocketOption.REUSE_ADDRESS, false);
         verify(socketChannel).set(IOSocketOption.KEEP_ALIVE, false);
         verify(socketChannel).set(IOSocketOption.TCP_NO_DELAY, false);
