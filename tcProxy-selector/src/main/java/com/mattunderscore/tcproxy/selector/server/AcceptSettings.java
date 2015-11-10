23c57f54078da449c8f7e49208acb5f91980c918
Matt Champion <mattunderscorechampion@gmail.com>
2015-11-10 21:35:34 +0000

    Renamed method to get ports to listen on.

--- tcProxy-selector/src/main/java/com/mattunderscore/tcproxy/selector/server/AcceptSettings.java
+++ tcProxy-selector/src/main/java/com/mattunderscore/tcproxy/selector/server/AcceptSettings.java
@@ -39,7 +39,7 @@ public final class AcceptSettings {
         this.listenOn = listenOn;
     }
 
-    public Iterable<Integer> listenOn() {
+    public Iterable<Integer> getListenOn() {
         return listenOn;
     }
 
