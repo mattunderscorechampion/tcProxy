f2550495828dfce9af6e47196afa9922186b1f66
Matt Champion <mattunderscorechampion@gmail.com>
2015-11-10 21:11:30 +0000

    Added a restartable thread.

---
+++ tcProxy-selector/src/main/java/com/mattunderscore/tcproxy/selector/threads/RestartableThread.java
@@ -0,0 +1,86 @@
+/* Copyright © 2015 Matthew Champion
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ * Neither the name of mattunderscore.com nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL MATTHEW CHAMPION BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package com.mattunderscore.tcproxy.selector.threads;
+
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * This "thread" can be started and stopped. A new {@link Thread} is created every time this "thread" is started.
+ * @author Matt Champion on 10/11/2015
+ */
+public final class RestartableThread implements StartStopLifecycle {
+    private final LifecycleState state = new LifecycleState();
+    private final ThreadFactory threadFactory;
+    private final InnerTask innerTask;
+
+    public RestartableThread(ThreadFactory threadFactory, StartStopLifecycle task) {
+        this.threadFactory = threadFactory;
+        this.innerTask = new InnerTask(state, task);
+    }
+
+    @Override
+    public void start() {
+        state.beginStartup();
+
+        threadFactory.newThread(innerTask).start();
+    }
+
+    @Override
+    public void stop() {
+        state.beginShutdown();
+        innerTask.stop();
+    }
+
+    @Override
+    public void waitForRunning() throws InterruptedException {
+        state.waitForRunning();
+    }
+
+    @Override
+    public void waitForStopped() throws InterruptedException {
+        state.waitForStopped();
+    }
+
+    private static final class InnerTask implements Runnable {
+        private LifecycleState state;
+        private StartStopLifecycle task;
+
+        public InnerTask(LifecycleState state, StartStopLifecycle task) {
+            this.state = state;
+            this.task = task;
+        }
+
+        @Override
+        public void run() {
+            task.start();
+            state.endShutdown();
+        }
+
+        public void stop() {
+            task.stop();
+        }
+    }
+}
