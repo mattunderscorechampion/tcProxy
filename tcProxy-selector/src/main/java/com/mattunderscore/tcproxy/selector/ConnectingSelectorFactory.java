7be23d07a5b4fcefab1897b816ffeafb32821745
Matt Champion <mattunderscorechampion@gmail.com>
2015-11-14 13:13:19 +0000

    Improved the JavaDoc for ConnectingSelectorFactory.

--- tcProxy-selector/src/main/java/com/mattunderscore/tcproxy/selector/ConnectingSelectorFactory.java
+++ tcProxy-selector/src/main/java/com/mattunderscore/tcproxy/selector/ConnectingSelectorFactory.java
@@ -36,7 +36,7 @@ import com.mattunderscore.tcproxy.selector.server.SocketConfigurator;
 import com.mattunderscore.tcproxy.selector.task.ConnectionHandlerFactory;
 
 /**
- * Factory for {@link ConnectingSelector}s.
+ * Factory for {@link ConnectingSelector}.
  * @author Matt Champion on 09/11/2015
  */
 public final class ConnectingSelectorFactory implements SelectorFactory<ConnectingSelector> {
