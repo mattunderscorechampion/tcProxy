7a0cc435ade666c31e6b83c405060c2d05d2f69f
Matt Champion <mattunderscorechampion@gmail.com>
2016-01-13 21:35:17 +0000

    Added deserialisation results.

---
+++ tcProxy-io-api/src/main/java/com/mattunderscore/tcproxy/io/serialisation/NeedsMoreDataResult.java
@@ -0,0 +1,65 @@
+/* Copyright © 2016 Matthew Champion
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ * Neither the name of mattunderscore.com nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL MATTHEW CHAMPION BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package com.mattunderscore.tcproxy.io.serialisation;
+
+/**
+ * @author Matt Champion on 13/01/16
+ */
+public final class NeedsMoreDataResult<T> implements Deserialiser.Result<T> {
+    private static final NeedsMoreDataResult INSTANCE = new NeedsMoreDataResult();
+
+    private NeedsMoreDataResult() {
+    }
+
+    @Override
+    public boolean needsMoreData() {
+        return true;
+    }
+
+    @Override
+    public boolean hasMoreData() {
+        return false;
+    }
+
+    @Override
+    public boolean notDeserialisable() {
+        return false;
+    }
+
+    @Override
+    public T result() {
+        throw new IllegalStateException("No result");
+    }
+
+    /**
+     * Create result.
+     * @param <T> The type of result
+     * @return The result
+     */
+    public static final <T> Deserialiser.Result<T> create() {
+        return INSTANCE;
+    }
+}
