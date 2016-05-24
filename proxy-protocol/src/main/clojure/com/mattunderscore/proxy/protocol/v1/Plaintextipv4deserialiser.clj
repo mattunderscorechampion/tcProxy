ecb793b029bf4b451a48634b699aeeb86f8a7bfe
Matt Champion <mattunderscorechampion@gmail.com>
2016-05-24 22:07:14 +0100

    Successfuly deserialise a plain text IPv4 using clojure.

--- proxy-protocol/src/main/clojure/com/mattunderscore/proxy/protocol/v1/Plaintextipv4deserialiser.clj
+++ proxy-protocol/src/main/clojure/com/mattunderscore/proxy/protocol/v1/Plaintextipv4deserialiser.clj
@@ -1,6 +1,7 @@
 
 (ns com.mattunderscore.proxy.protocol.v1.Plaintextipv4deserialiser
-  (import [com.mattunderscore.tcproxy.io.serialisation AbstractByteBufferDeserialiser NotDeserialisableResult NeedsMoreDataResult])
+  (import [com.mattunderscore.tcproxy.io.serialisation AbstractByteBufferDeserialiser NotDeserialisableResult NeedsMoreDataResult DeserialisationResult]
+          [java.net.Inet4Address])
   (:gen-class
     :extends com.mattunderscore.tcproxy.io.serialisation.AbstractByteBufferDeserialiser))
 
@@ -11,6 +12,10 @@
   (or (<= (int \0) byte (int \9))
       (is-group-separator byte)))
 
+(defn- create-address-result-from-context [context remaining]
+  (let [bytes (into-array Byte/TYPE (map (fn [group] (Integer/parseInt (clojure.string/join (map char group)))) (:groups context)))]
+    (DeserialisationResult/create (java.net.Inet4Address/getByAddress bytes) (:processed context) (nil? (first remaining)))))
+
 (defn- process-next-byte [seq context]
   (if-let [byte (first seq)]
     (if (is-valid-byte byte)
@@ -20,19 +25,33 @@
                    (= 4 (count (:groups context)))))
         ; Group separator without pending group or too many groups
         (NotDeserialisableResult/create (+ (:processed context) 1))
-        (if (= (count (:pending context)) 3)
+        (if (= (count (:pending context)) 4)
           ; Group too long
           (NotDeserialisableResult/create (+ (:processed context) 1))
           (if (is-group-separator byte)
+            ; Pack new group
             (process-next-byte (rest seq) (-> context
                                               (update-in [:processed] + 1)
                                               (update-in [:groups] conj (:pending context))
                                               (assoc :pending [])))
+            ; Add to pending group
             (process-next-byte (rest seq) (-> context
                                               (update-in [:processed] + 1)
                                               (update-in [:pending] conj byte))))))
-      (NotDeserialisableResult/create (+ (:processed context) 1)))
-    (NeedsMoreDataResult/INSTANCE)))
+      (if (= 4 (count (:groups context)))
+        ; Create the address
+        (create-address-result-from-context context (rest seq))
+        ; Ended early
+        (NotDeserialisableResult/create (+ (:processed context) 1))))
+    ; No more data
+    (if (and (= 3 (count (:groups context)))
+             (> (count (:pending context)) 0))
+            ; Create the address
+            (create-address-result-from-context (-> context
+                                                    (update-in [:groups] conj (:pending context))
+                                                    (assoc :pending [])) (rest seq))
+            ; Ended early
+            (NeedsMoreDataResult/INSTANCE))))
 
 (defn- read-from-sequence [seq]
   (process-next-byte seq {:processed 0 :groups [] :pending []}))
